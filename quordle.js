6656feaf10d6be073864f3f1a3beaec723b73aed
Schwer <42115696+itsschwer@users.noreply.github.com>
2022-04-14 00:56:27 +1000

    Use const instead of let where applicable

--- quordle.js
+++ quordle.js
@@ -3,11 +3,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 function format() {
-    let split = document.querySelector('textarea').innerHTML.split(/\r?\n/);
-    let guesses = getGuesses(split);
-    let colors = flattenColors(split);
-    let words = getWords();
-    let header = `${split[0]} ${guesses[0]},${guesses[1]},${guesses[2]},${guesses[3]}/9`;
+    const split = document.querySelector('textarea').innerHTML.split(/\r?\n/);
+    const guesses = getGuesses(split);
+    const colors = flattenColors(split);
+    const words = getWords();
+    const header = `${split[0]} ${guesses[0]},${guesses[1]},${guesses[2]},${guesses[3]}/9`;
 
     let out = `${header}\n`;
     for (let i = 0; i < words.length; i++) {
@@ -21,7 +21,7 @@ function format() {
 }
 
 function getGuesses(split) {
-    let array = new Array(4);
+    const array = new Array(4);
     array[0] = convert(split[1][0]);
     array[1] = (array[0] == 'X') ? convert(split[1][2]) : convert(split[1][3]);
     array[2] = convert(split[2][0]);
@@ -32,17 +32,15 @@ function getGuesses(split) {
 function convert(guess) { return (guess == '\uD83D') ? 'X' : guess; }
 
 function flattenColors(split) {
-    let array = new Array(9);
-    let br = split.findIndex(w => w == '');
+    const array = new Array(9);
+    const br = split.findIndex(w => w == '');
     const start = 4;
-    let rows1 = br - start;
-    let rows2 = split.length - rows1 - 1;
 
     for (let i = 0; i < array.length; i++) {
         array[i] = (i + start < br) ? `${split[i + start]} ` : '⬛⬛⬛⬛⬛ ⬛⬛⬛⬛⬛ ';
     }
     for (let i = 0; i < array.length; i++) {
-        let j = i + br + 1;
+        const j = i + br + 1;
         array[i] += (j < split.length && split[j] != "") ? split[j] : '⬛⬛⬛⬛⬛ ⬛⬛⬛⬛⬛';
     }
 
@@ -50,11 +48,11 @@ function flattenColors(split) {
 }
 
 function getWords() {
-    let words = new Array(9);
-    let boards = document.querySelectorAll('div.flex.flex-col.flex-auto.p-1');
+    const words = new Array(9);
+    const boards = document.querySelectorAll('div.flex.flex-col.flex-auto.p-1');
 
     for (let i = 0, r = 0; i < boards.length; i++) {
-        let rows = boards[i].querySelectorAll('div.flex.w-full');
+        const rows = boards[i].querySelectorAll('div.flex.w-full');
         for (let j = r; j < rows.length; j++) {
             if (rows[j].ariaLabel[12] == ' ') {
                 words[r] = rows[j].ariaLabel.slice(13, 18);
