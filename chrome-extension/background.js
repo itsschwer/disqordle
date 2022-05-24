b53ae6a50cf1b94289f6e3a84c9c0556324d619e
Schwer <42115696+itsschwer@users.noreply.github.com>
2022-05-25 00:22:21 +1000

    Add browser action for opening tabs

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,3 +1,9 @@
+chrome.browserAction.onClicked.addListener((tab) => {
+    chrome.tabs.create({ url: "https://www.nytimes.com/games/wordle/index.html", active: true });
+    chrome.tabs.create({ url: "https://zaratustra.itch.io/dordle", active: false });
+    chrome.tabs.create({ url: "https://www.quordle.com", active: false });
+});
+
 chrome.contextMenus.create({
     documentUrlPatterns: [ "*://www.quordle.com/*" ],
     title: "Copy formatted Quordle results",
